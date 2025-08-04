e820ff78d3c49d299bb60bbc3558e18c2ec6bc9a
Ronny Bezerra <arbo09427@gmail.com>
2025-08-04 10:37:21 -0300

    Project's finalization

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function somar(event) {
     const numero1 = document.querySelector("#numero-1").valueAsNumber
     const numero2 = document.querySelector("#numero-2").valueAsNumber
 
-    const soma = numero1 + numero2
+   const soma = numero1 + numero2
 
-    const paragrafoResultado = document.querySelector("#resultado")
+    const paragrafoResultado = document.querySelector("#resultado-soma")
     paragrafoResultado.innerHTML = `Soma: ${soma}`
 }
